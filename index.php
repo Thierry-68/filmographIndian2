2721ff6748ccf4cd72a7a661507a3782aef6baa5
Thierry-68 <thierry.sandre68@gmail.com>
2021-03-20 00:10:04 +0100

    TS - Clean the solution

--- index.php
+++ index.php
@@ -11,22 +11,6 @@ Dans le film movie_title, les principaux acteurs sont :
 name_of_the_actor_1, name_of_the_actor_2, name_of_the_actor_3.
 */
 
-/*
-$movie_title[]="Indiana Jones and the Last Crusade";
-$movie_title[]="Indiana Jones and the Temple of Doom";
-$movie_title[]="Raiders of the Lost Ark";
-
-$film_Indian[$movie_title[0]]["name_of_the_actor_1"]="Harrison Ford";
-$film_Indian[$movie_title[0]]["name_of_the_actor_2"]="Sean Connery";
-$film_Indian[$movie_title[0]]["name_of_the_actor_3"]="Alison Doody";
-$film_Indian[$movie_title[1]]["name_of_the_actor_1"]="Harrison Ford";
-$film_Indian[$movie_title[1]]["name_of_the_actor_2"]="Kate Capshaw";
-$film_Indian[$movie_title[1]]["name_of_the_actor_3"]="Ke Huy Quan";
-$film_Indian[$movie_title[2]]["name_of_the_actor_1"]="Karen Allen";
-$film_Indian[$movie_title[2]]["name_of_the_actor_2"]="Kate Capshaw";,
-$film_Indian[$movie_title[2]]["name_of_the_actor_3"]="Paul Freeman";
-*/
-
 $film_Indian = [
     "Indiana Jones and the Last Crusade" => ["Harrison Ford","Sean Connery","Alison Doody"],
     "Indiana Jones and the Temple of Doom" => ["Harrison Ford","Kate Capshaw","Ke Huy Quan"],
